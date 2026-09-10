277a60e4390e566c60d974187e8faab15841301e
Relaxgarments12 <mdaltafansari740@gmail.com>
2026-09-10 23:16:32 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://dqzvheupzchiwzsasthk.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable__wc54tX_f_pCwUflxMCCIQ_cJUfOfJF
+window.SUPABASE_ANON_KEY = "sb_publishable_sb_publishable__wc54tX_f_pCwUflxMCCIQ_cJUfOfJF
